f68d4a86b7154baf38032ad463a9241d46ed3191
Cat <menten.c@northeastern.edu>
2024-10-26 10:19:19 -0400

    Create rotary_encoder.ino

---
+++ testing/rotary_encoder/rotary_encoder.ino
@@ -0,0 +1,37 @@
+const int PIN_CS = 11;
+const int PIN_CLOCK = 6;
+const int PIN_DATA = 7;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_CS, OUTPUT);
+  pinMode(PIN_CLOCK, OUTPUT);
+  pinMode(PIN_DATA, INPUT);
+
+  digitalWrite(PIN_CLOCK, HIGH);
+  digitalWrite(PIN_CS, LOW);
+}
+
+
+//byte stream[16];
+void loop() {
+
+  digitalWrite(PIN_CS, HIGH);
+  digitalWrite(PIN_CS, LOW);
+  int pos = 0;
+  for (int i=0; i<10; i++) {
+    digitalWrite(PIN_CLOCK, LOW);
+    digitalWrite(PIN_CLOCK, HIGH);
+   
+    pos= pos | digitalRead(PIN_DATA);
+    if(i<9) pos = pos << 1;
+  }
+  for (int i=0; i<6; i++) {
+    digitalWrite(PIN_CLOCK, LOW);
+    digitalWrite(PIN_CLOCK, HIGH);
+  }
+  digitalWrite(PIN_CLOCK, LOW);
+  digitalWrite(PIN_CLOCK, HIGH);
+  Serial.println(pos);
+  delay(100);
+}
